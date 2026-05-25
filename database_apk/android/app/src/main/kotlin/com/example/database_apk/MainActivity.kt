5512d00bd1cfc86f99976e76e80847b076e3c4d1
turky <160696449+Tiiilod@users.noreply.github.com>
2026-05-25 13:30:37 +0700

    .

--- database_apk/android/app/src/main/kotlin/com/example/database_apk/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.database_apk
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- database_apk/lib/models/prayer_time.dart
+++
@@ -1,7 +0,0 @@
-// models/prayer_time.dart
-class PrayerTime {
-  final String name;
-  final String time;
-
-  PrayerTime({required this.name, required this.time});
-}
